1561e0c76a98b51120e60b69073c8c6b4f3cc800
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-07 03:12:00 -0600

    Agregue dos nuevos archivos js que son el footer y las categorias de blogs

---
+++ src/components/Categorias.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Categorias(props) {
+    return (
+        <div className="categorias mt-5">
+            <div class="container">
+                <h3 className="text-right">Canciones clásicas del rock</h3>
+                <div class="row border">
+                    <div class="col-md-4 border">
+                    <img src="https://images.unsplash.com/photo-1487954152950-e0584673b7f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" className="img-thumbnail mt-2"/>
+                        <h4 class="service-heading mt-2 text-center">Led Zeppelin - Stairway to Heaven</h4>
+                        <p class="text-muted text-justify">Led Zeppelin fue un grupo británico de rock, considerado uno de los mejores grupos musicales de la historia. Fue fundado en 1968 por el guitarrista Jimmy Page, quien había pertenecido a The Yardbirds</p>
+                        <p class="text-muted text-right">05/02/2020</p>
+                        <button type="button" className="btn btn-warning text-white">Leer más</button>                        
+                    </div>                    
+                    <div class="col-md-4 border">
+                    <img src="https://images.unsplash.com/photo-1487954152950-e0584673b7f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" className="img-thumbnail mt-2"/>
+                        <h4 class="service-heading mt-2 text-center">Led Zeppelin - Stairway to Heaven</h4>
+                        <p class="text-muted text-justify">Led Zeppelin fue un grupo británico de rock, considerado uno de los mejores grupos musicales de la historia. Fue fundado en 1968 por el guitarrista Jimmy Page, quien había pertenecido a The Yardbirds</p>
+                        <p class="text-muted text-right">05/02/2020</p>
+                        <button type="button" className="btn btn-warning text-white">Leer más</button>                        
+                    </div>                    
+                    <div class="col-md-4 border">
+                    <img src="https://images.unsplash.com/photo-1487954152950-e0584673b7f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" className="img-thumbnail mt-2"/>
+                        <h4 class="service-heading mt-2 text-center">Led Zeppelin - Stairway to Heaven</h4>
+                        <p class="text-muted text-justify">Led Zeppelin fue un grupo británico de rock, considerado uno de los mejores grupos musicales de la historia. Fue fundado en 1968 por el guitarrista Jimmy Page, quien había pertenecido a The Yardbirds</p>
+                        <p class="text-muted text-right">05/02/2020</p>
+                        <button type="button" className="btn btn-warning text-white">Leer más</button>                        
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Categorias;
